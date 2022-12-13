7caf9bacd5eb1b9f2a460a4b4ab131709582fab3
kanonlas <kanonlas_r@cmu.ac.th>
2022-12-13 15:41:51 +0700

    finishtest 1

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,10 +5,22 @@ using namespace std;
 int main(){
 	
 	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	cout << "k = ";
+	cin >> k;
+
+	if(k > 0 ){
+
+   while(i < 5000){
 		P[i] = 2*i+1;
+		result
 		i++;
 	}
-	
+
+	cout << "result = " << result; 
+    
+	}else{
+    cout << "Invalid input!!!";
+
+	}
 	return 0;
 }
